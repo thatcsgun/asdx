dd15629c3bbfcceccf71066342d0647dece00bde
thatcsgun <57810923+thatcsgun@users.noreply.github.com>
2019-11-15 23:39:59 +0500

    Add files via upload

---
+++ tehreem.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int n;
+  
+  cout << "Enter a Number: ";
+  cin >> n;
+  
+      if(n % 2 == 0){
+         
+         
+         for (int i = 0; i < 5; i++) {
+                  n = n/2;
+            cout << n+"okl";
+      }
+      }
+      else if(n % 2 != 0){
+         
+         cout << n + "is not a prime number";
+      }
+          
+          
+      
+  
+   
+      
+ 
+}
